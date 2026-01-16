6fb9ffe93242a09843b4df47602c00e298e90b7f
sneema-design <sneema@gammastack.com>
2026-01-16 17:18:00 +0530

    updated array immutable method file

---
+++ array_immutable_method.js
@@ -0,0 +1,9 @@
+const arr=["hghg","kkeke","heheh"];
+const neww=arr.toReversed()
+console.log(neww)
+
+const sortedd=arr.toSorted((a,b)=>a-b);
+console.log(sortedd)
+
+const silicedd=arr.toSpliced()
+console.log(silicedd)
