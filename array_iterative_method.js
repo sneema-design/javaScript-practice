1cc4ef003bc56707ee9ffa22c0ef885be1533efc
sneema-design <sneema@gammastack.com>
2026-01-16 17:32:38 +0530

    array_iterative method file add

---
+++ array_iterative_method.js
@@ -0,0 +1,11 @@
+const h=[121,1,1,22]
+
+const mapp = h.map(item=>item*2);
+console.log(mapp)
+
+
+const filterr= h.filter(item=> item>100)
+console.log(filterr)
+
+
+h.forEach(item=> console.log(item))
